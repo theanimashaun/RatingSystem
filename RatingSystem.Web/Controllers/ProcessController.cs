2ef328ad3eeb771b4d0301fa174f672ce97153a4
Damilola Animashaun <damilola@sure.group>
2019-02-27 01:35:16 +0100

    Implement Bayesian Rating

--- RatingSystem.Web/Controllers/ProcessController.cs
+++ RatingSystem.Web/Controllers/ProcessController.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using RatingSystem.Web.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace RatingSystem.Web.Controllers
 {
@@ -11,5 +9,32 @@ namespace RatingSystem.Web.Controllers
     [ApiController]
     public class ProcessController : ControllerBase
     {
+        [HttpPost("")]
+        public IActionResult Rank([FromBody]List<Book> books)
+        {
+            if (books == null || books.Count < 2)
+            {
+                return BadRequest("Please provide a list of books.");
+            }
+
+            return Ok(RankBooks(books));
+        }
+
+        private List<Book> RankBooks(List<Book> books)
+        {
+            var averageRaters = books.Average(x => x.TotalRaters);
+            var averageRating = books.Average(x => x.AverageRating);
+
+            return books.Select(x => CalculateRanking(x, averageRaters, averageRating))
+                .OrderByDescending(x => x.Ranking)
+                .ToList();
+        }
+
+        private Book CalculateRanking(Book book, double averageNumberOfRaters, double averageRating)
+        {
+            book.Ranking = ((averageNumberOfRaters * averageRating) + (book.TotalRaters * book.AverageRating)) /
+                           (averageNumberOfRaters + book.TotalRaters);
+            return book;
+        }
     }
 }
---
+++ RatingSystem.Web/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RatingSystem.Web.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public double AverageRating { get; set; }
+        public int TotalRaters { get; set; }
+        public double Ranking { get; set; }
+    }
+}
